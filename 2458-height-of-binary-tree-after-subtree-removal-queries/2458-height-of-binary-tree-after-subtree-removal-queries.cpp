a5b14f47ef2212d2157c15988e39b797bd70d83b
RakshulMahajan <140340277+RakshulMahajan@users.noreply.github.com>
2024-10-26 21:22:08 +0530

    Time: 466 ms (65.77%), Space: 401.3 MB (16.08%) - LeetHub

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,int> l,r,ans;
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        int le = height(root->left);
+        int re = height(root->right);
+        l[root->val] = le;                 // Storing maximum height in the left side of current node
+        r[root->val] = re;                 // Storing maximum height in the right side of current node
+        return max(le,re)+1;      
+    }
+    void fun(TreeNode* root,int MaxSoFar,int depth){
+        if(root==NULL) return;
+
+        ans[root->val] = MaxSoFar;
+        fun(root->left,max(MaxSoFar,depth+r[root->val]),depth+1);
+        fun(root->right,max(MaxSoFar,depth+l[root->val]),depth+1);
+    }
+    vector<int> treeQueries(TreeNode* root, vector<int>& q) {
+        height(root);
+        
+        fun(root->left,r[root->val],1);               // Calling on left side and passing the maximum height of right side of root
+        fun(root->right,l[root->val],1);              // Calling on right side and passing the maximum height of left side of root
+        
+        vector<int> res(q.size());
+        for(int i = 0;i<q.size();i++){
+            res[i] = ans[q[i]];
+        }
+        return res;
+    }
+};
