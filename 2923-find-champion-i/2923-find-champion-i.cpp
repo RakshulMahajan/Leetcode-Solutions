7a6c05d0019ff6e9d3ea4980b29ff54a37ff2abd
RakshulMahajan <140340277+RakshulMahajan@users.noreply.github.com>
2024-11-26 10:51:21 +0530

    Time: 4 ms (18.18%), Space: 40.2 MB (91.84%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for(int i = 0; i<n; i++){
+            int c = 0;
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1){
+                    c++;
+                }
+                if(c == n-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
